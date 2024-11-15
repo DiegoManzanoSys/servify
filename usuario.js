566a22d2412151d97fa416688696d744eb43680f
DiegoManzanoSys <diegodjmanzano1@hotmail.com>
2024-11-15 09:31:49 -0600

    funciones asíncronas

--- usuario.js
+++ usuario.js
@@ -2,17 +2,13 @@
 const db = require('./db');  // Importa la conexión a la base de datos
 
 // Crear un nuevo usuario
-const crearUsuario = (req, res) => {
-  // Imprimir el cuerpo de la solicitud para depuración
 
+const crearUsuario = async(req, res) => {
 
   const {id_usuario, p_nombre, p_apellidos, p_email, fecha_nacimiento, domicilio } = req.body;
 
-
-
-  // Validar que todos los campos requeridos estén presentes
   if (!p_nombre || !p_apellidos || !p_email || !fecha_nacimiento || !domicilio) {
-      return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+      return res.status(400).json({ error: 'Llene todos los campos' });
   }
 
 
@@ -31,22 +27,20 @@ const crearUsuario = (req, res) => {
 
 
 // Obtener todos los usuarios
-const obtenerUsuarios = (req, res) => {
+const obtenerUsuarios = async(req, res) => {
   const query = 'CALL sp_read_all_users()';
   
   db.query(query, (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    res.status(200).json(results[0]); // El resultado viene en un arreglo, así que accedemos al primer elemento
+    res.status(200).json(results[0]); 
   });
 };
 
 // Obtener un usuario por ID
-const obtenerUsuarioPorId = (req, res) => {
+const obtenerUsuarioPorId = async(req, res) => {
   const idusuario = req.params.id;
-  
-  // Llamar al procedimiento almacenado sp_read_usuario
   const query = 'CALL sp_read_usuario(?)';
   
   db.query(query, [idusuario], (err, results) => {
@@ -56,12 +50,12 @@ const obtenerUsuarioPorId = (req, res) => {
     if (results[0].length === 0) {
       return res.status(404).json({ message: 'Usuario no encontrado' });
     }
-    res.status(200).json(results[0][0]); // Retornar el primer resultado del usuario
+    res.status(200).json(results[0][0]); 
   });
 };
 
 // Actualizar un usuario
-const actualizarUsuario = (req, res) => {
+const actualizarUsuario = async(req, res) => {
   const { id } = req.params;
   const { p_nombre, p_apellidos, p_email, fecha_nacimiento, domicilio } = req.body;
   const query = 'CALL sp_update_usuario(?, ?, ?, ?, ?, ?)';
@@ -79,7 +73,7 @@ const actualizarUsuario = (req, res) => {
 };
 
 // Eliminar un usuario
-const eliminarUsuario = (req, res) => {
+const eliminarUsuario = async(req, res) => {
   const { id } = req.params;
   const query = 'CALL sp_delete_usuario(?)';
 
